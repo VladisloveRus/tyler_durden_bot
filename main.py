f3d99192c0a3980da9bea7e4ee6f447fe381aa69
Vladislav Cherepanov <V.V.Cherepanov@bk.ru>
2025-02-12 21:08:52 +0500

    get ready for PostgreSQL

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import webbrowser
-import telebot
 import sqlite3
-import os
+import telebot
+import webbrowser
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))  # type: ignore
@@ -15,7 +15,7 @@ def create_db(message):
     conn = sqlite3.connect("tyler_durden_database.sql")
     cur = conn.cursor()
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS users (id integer primary key autoincrement, tg_id integer not null unique, name varchar(50))"
+        "CREATE TABLE IF NOT EXISTS users (id integer primary key autoincrement, tg_id integer, name varchar(50))"
     )
     conn.commit()
     cur.close()
@@ -36,23 +36,17 @@ def update_tg_id_set():
         tg_id_set.add(tg_id)
 
 
-#        print(
-#            f"tg_id_list = {tg_id_list} : {type(tg_id_list)} , tg_id = {tg_id} : {type(tg_id)}, tg_id_set = {tg_id_set} : {type(tg_id_set)}"
-#        )
-
-
 def check_user(message):
     """Идентификация зарегистрированного пользователя"""
     if message.chat.id not in tg_id_set:
-        print(message.chat.id, tg_id_set)
         bot.reply_to(message, "Привет. Мы не знакомы, представься.")
         bot.register_next_step_handler(message, register_user)
     else:
         conn = sqlite3.connect("tyler_durden_database.sql")
         cur = conn.cursor()
         db_name = cur.execute(
-            "SELECT name FROM users WHERE tg_id = ('%i') LIMIT 1"
-            % (message.chat.id)
+            "SELECT name FROM users WHERE tg_id = ? LIMIT 1",
+            (message.chat.id,),
         )
         name = db_name.fetchone()[0]
         bot.reply_to(message, f"Привет, {name}")
@@ -60,23 +54,24 @@ def check_user(message):
 
 @bot.message_handler(commands=["reg"])
 def register_user(message):
+    """Функция регистрации нового пользователя"""
     name = message.text.strip()
     tg_id = message.chat.id
     conn = sqlite3.connect("tyler_durden_database.sql")
     cur = conn.cursor()
     try:
         cur.execute(
-            "INSERT INTO users (name, tg_id) VALUES ('%s', '%i')"
-            % (name, tg_id)
+            "INSERT INTO users (tg_id, name) VALUES (?, ?)", (tg_id, name)
         )
     except sqlite3.IntegrityError:
         bot.send_message(
             message.chat.id,
-            f"Что-то пошло не так. Напиши по этому поводу {os.getenv("CREATOR")}, он разберётся.",
+            f"Что-то пошло не так. Напиши по этому поводу {os.getenv('CREATOR')}, он разберётся.",
         )
     else:
         conn.commit()
         update_tg_id_set()
+        # Начало временного функционала
         markup = telebot.types.InlineKeyboardMarkup()
         markup.add(
             telebot.types.InlineKeyboardButton(
@@ -86,10 +81,11 @@ def register_user(message):
                 "Список telegram id", callback_data="tg_id_set"
             ),
         )
+        # Конец временного функционала
         bot.send_message(
             message.chat.id,
             "Хорошо. Я - Тайлер Дёрден.",
-            reply_markup=markup,
+            reply_markup=markup,  # Элемент временного функционала
         )
     finally:
         cur.close()
@@ -98,9 +94,11 @@ def register_user(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
+    """Функционал кнопок"""
     conn = sqlite3.connect("tyler_durden_database.sql")
     cur = conn.cursor()
     if call.data == "users":
+        """Коллбэк, отвечающий за вызов списка пользователей"""
         cur.execute("SELECT * FROM users")
         user_list = cur.fetchall()
         info = ""
@@ -110,54 +108,42 @@ def callback(call):
     cur.close()
     conn.close()
     if call.data == "tg_id_set":
+        """Коллбэк, отвечающий за вызов множества tg_id_set"""
         bot.send_message(call.message.chat.id, str(tg_id_set))
-
-
-#    bot.reply_to(
-#        message,
-#        f"Значит, тебя зовут {name}? Хорошо. Я - Тайлер. Тайлер Дёрден.",
-#    )
 
 
 @bot.message_handler(commands=["start", "hello"])
 def start(message):
+    """Обработка стартовых сообщений"""
+    # Тестовый функционал
     create_db(message)
     update_tg_id_set()
     check_user(message)
 
 
-#    markup = telebot.types.InlineKeyboardMarkup()
-#    markup.add(
-#        telebot.types.InlineKeyboardButton(
-#            "Подписаться на канал", url=os.getenv("INVITE")
-#        )
-#    )
-#    bot.send_message(
-#        message.chat.id,
-#        "Привет, я - <b>Тайлер Дёрден</b>",
-#        parse_mode="html",
-#        reply_markup=markup,
-#    )
-
-
 @bot.message_handler(commands=["link"])
 def link(message):
-    bot.send_message(message.chat.id, "Добро пожаловать.")
-    webbrowser.open(os.getenv("INVITE"))
+    """Функция получения ссылки-приглашения"""
+    bot.send_message(message.chat.id, "Добро пожаловать в клуб.")
+    webbrowser.open(os.getenv("INVITE"))  # type: ignore
 
 
 @bot.message_handler(content_types=["photo", "video"])
 def get_photo(message):
+    """Реакция бота на фото и видео"""
     bot.reply_to(message, "Я не буду это смотреть.")
 
 
 @bot.message_handler(content_types=["audio"])
 def get_audio(message):
+    """Реакция бота на аудио"""
     bot.reply_to(message, "Я не буду это слушать.")
 
 
 @bot.message_handler()
 def messaging(message):
+    """Реакция на другие сообщения"""
+    # Тестовый функционал
     if message.text == "Покажи мне всё":
         bot.send_message(message.chat.id, message)
     else:
