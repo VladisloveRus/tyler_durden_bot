d9a61f7e5aee1f2f6565c16397f98fc5e9e9b993
Vladislav Cherepanov <V.V.Cherepanov@bk.ru>
2025-03-18 16:36:11 +0500

    PostgreSQLed

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # tyler_durden_bot
 Бот для "Настольного клуба"
+
+Инструкция. Внешний вид ридми приведу в порядок чуть позже.
+
+1. Создаём ВО, запускаем его, устанавливаем зависимости, создаём .env по подобию .env.example
+-python -m venv venv
+-. venv/scripts/activate
+-pip install -r requirements.txt
+
+2. Запускаем код, чтобы создалась БД, затем останавливаем
+-python main.py
+
+3. Подключаемся к БД
+-psql -U postgres
+вводим пароль
+-CREATE DATABASE tyler_durden_db;
+-CREATE USER bot_user WITH PASSWORD 'password';
+-GRANT ALL PRIVILEGES ON DATABASE tyler_durden_db TO bot_user;
+-GRANT ALL PRIVILEGES ON SCHEMA public TO bot_user;
+-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO bot_user;
+
+4. Запускаем код, пользуемся функционалом (или его отсутствием на данный момент)
+-python main.py
--- main.py
+++ main.py
@@ -1,122 +1,125 @@
-import sqlite3
 import telebot
 import webbrowser
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 
 load_dotenv()
 bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))  # type: ignore
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)  # type: ignore
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 tg_id_set = set()
 
 
-@bot.message_handler(commands=["create_DB"])
-def create_db(message):
-    """Функция создания базы данных пользователей"""
-    conn = sqlite3.connect("tyler_durden_database.sql")
-    cur = conn.cursor()
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS users (id integer primary key autoincrement, tg_id integer, name varchar(50))"
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
-    bot.reply_to(message, "Да записываю я, записываю!")
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tg_id = Column(Integer, unique=True)
+    name = Column(String(50))
+
+    def __repr__(self):
+        return f"<User(name={self.name}, tg_id={self.tg_id})>"
+
+
+Base.metadata.create_all(engine)
 
 
 def update_tg_id_set():
     """Обновление глобального множества tg_id_set для идентификации зарегистрированных пользователей"""
     global tg_id_set
-    conn = sqlite3.connect("tyler_durden_database.sql")
-    cur = conn.cursor()
-    cur.execute("SELECT tg_id FROM users")
-    tg_id_list = cur.fetchall()
-    cur.close()
-    conn.close()
-    for tg_id in tg_id_list[0]:
-        tg_id_set.add(tg_id)
+    try:
+        users = session.query(User).all()
+        tg_id_set = {user.tg_id for user in users}
+    except SQLAlchemyError as e:
+        print(f"Ошибка при обновлении tg_id_set: {e}")
 
 
 def check_user(message):
     """Идентификация зарегистрированного пользователя"""
     if message.chat.id not in tg_id_set:
-        bot.reply_to(message, "Привет. Мы не знакомы, представься.")
-        bot.register_next_step_handler(message, register_user)
-    else:
-        conn = sqlite3.connect("tyler_durden_database.sql")
-        cur = conn.cursor()
-        db_name = cur.execute(
-            "SELECT name FROM users WHERE tg_id = ? LIMIT 1",
-            (message.chat.id,),
+        answer = bot.reply_to(
+            message, "Привет. Мы не знакомы, представься. Только имя."
         )
-        name = db_name.fetchone()[0]
-        bot.reply_to(message, f"Привет, {name}")
+        bot.register_next_step_handler(answer, register_user)
+    else:
+        try:
+            user = (
+                session.query(User).filter_by(tg_id=message.chat.id).first()
+            )
+            if user:
+                bot.reply_to(message, f"Привет, {user.name}")
+        except SQLAlchemyError as e:
+            print(f"Ошибка при проверке пользователя: {e}")
 
 
 @bot.message_handler(commands=["reg"])
 def register_user(message):
-    """Функция регистрации нового пользователя"""
+    """Регистрация нового пользователя"""
     name = message.text.strip()
     tg_id = message.chat.id
-    conn = sqlite3.connect("tyler_durden_database.sql")
-    cur = conn.cursor()
+    existing_user = session.query(User).filter_by(tg_id=tg_id).first()
     try:
-        cur.execute(
-            "INSERT INTO users (tg_id, name) VALUES (?, ?)", (tg_id, name)
-        )
-    except sqlite3.IntegrityError:
-        bot.send_message(
-            message.chat.id,
-            f"Что-то пошло не так. Напиши по этому поводу {os.getenv('CREATOR')}, он разберётся.",
-        )
-    else:
-        conn.commit()
-        update_tg_id_set()
-        # Начало временного функционала
-        markup = telebot.types.InlineKeyboardMarkup()
-        markup.add(
-            telebot.types.InlineKeyboardButton(
-                "Список пользователей", callback_data="users"
-            ),
-            telebot.types.InlineKeyboardButton(
-                "Список telegram id", callback_data="tg_id_set"
-            ),
-        )
-        # Конец временного функционала
-        bot.send_message(
-            message.chat.id,
-            "Хорошо. Я - Тайлер Дёрден.",
-            reply_markup=markup,  # Элемент временного функционала
-        )
-    finally:
-        cur.close()
-        conn.close()
+        if existing_user:
+            bot.send_message(
+                message.chat.id,
+                f"Что-то пошло не так. Напиши по этому поводу {os.getenv('CREATOR')}, он разберётся.",
+            )
+        else:
+            new_user = User(tg_id=tg_id, name=name)
+            session.add(new_user)
+            session.commit()
+            update_tg_id_set()
+
+            # Начало временного функционала
+            markup = telebot.types.InlineKeyboardMarkup()
+            markup.add(
+                telebot.types.InlineKeyboardButton(
+                    "Список пользователей", callback_data="users"
+                ),
+                telebot.types.InlineKeyboardButton(
+                    "Список telegram id", callback_data="tg_id_set"
+                ),
+            )
+            # Конец временного функционала
+            bot.send_message(
+                message.chat.id,
+                "Хорошо. Я - Тайлер Дёрден.",
+                reply_markup=markup,  # Элемент временного функционала
+            )
+    except SQLAlchemyError as e:
+        print(f"Ошибка при регистрации пользователя: {e}")
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
     """Функционал кнопок"""
-    conn = sqlite3.connect("tyler_durden_database.sql")
-    cur = conn.cursor()
-    if call.data == "users":
-        """Коллбэк, отвечающий за вызов списка пользователей"""
-        cur.execute("SELECT * FROM users")
-        user_list = cur.fetchall()
-        info = ""
-        for unit in user_list:
-            info += f"Имя: {unit[2]}, Telegram id: {unit[1]}"
-        bot.send_message(call.message.chat.id, info)
-    cur.close()
-    conn.close()
-    if call.data == "tg_id_set":
-        """Коллбэк, отвечающий за вызов множества tg_id_set"""
-        bot.send_message(call.message.chat.id, str(tg_id_set))
+    try:
+        if call.data == "users":
+            """Коллбэк, отвечающий за вызов списка пользователей"""
+            users = session.query(User).all()
+            info = "\n".join(
+                [
+                    f"Имя: {user.name}, Telegram ID: {user.tg_id}"
+                    for user in users
+                ]
+            )
+            bot.send_message(call.message.chat.id, info)
+        elif call.data == "tg_id_set":
+            """Коллбэк, отвечающий за вызов множества tg_id_set"""
+            bot.send_message(call.message.chat.id, str(tg_id_set))
+    except SQLAlchemyError as e:
+        print(f"Ошибка при обработке коллбэка: {e}")
 
 
 @bot.message_handler(commands=["start", "hello"])
 def start(message):
     """Обработка стартовых сообщений"""
     # Тестовый функционал
-    create_db(message)
     update_tg_id_set()
     check_user(message)
 
